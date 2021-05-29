54c08508499731c0fa9d762c7d776dad7d39d3b7
stupidstolz <stupid.freelancer42@gmail.com>
2021-05-29 23:23:57 +0300

    fix numb function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const daysEl = document.getElementById("days");
-const hoursEl= document.getElementById("hours");
+const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
@@ -8,7 +8,7 @@ const NewYears = "1 Jan 2022";
 function countdown() {
     const newYearsDate = new Date(NewYears);
     const currentDate = new Date();
-    
+
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
@@ -17,14 +17,16 @@ function countdown() {
     const seconds = Math.floor(totalSeconds) % 60;
 
     daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours;
-    minsEl.innerHTML = mins;
-    secondsEl.innerHTML = seconds;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-//function formatTime(time) {
-// return time <10 ? ('0${time}')  // functon doesn't work
-// it's add 0 to 01-09 sec/mins
-//console.log(days, hours, mins, seconds)
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+// functon working now
+// it's add 0 to 01-09 sec/mins etc.
+//console.log(days, hours, mins, seconds) //testing numbers
 // initial call
 countdown();
 
